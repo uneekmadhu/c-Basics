7ef3532f2a5d4ed52c2d4cb93047b8a7e11bc08a
uneekmadhu <92910605+uneekmadhu@users.noreply.github.com>
2021-10-23 12:32:01 +0530

    Add files via upload

--- uneek.c
+++ uneek.c
@@ -1,8 +1,5 @@
 #include<stdio.h>
 void main()
 {
-	printf("uneek movie world");
- } 
- 
- 
- 
+	printf("STUDENT\tAGE\tCGPA\n\n\nUNEEK\t17\t8.0\n\nSANJAY\t19\t11.0\n\nVENKY\t20\t6.0\n\nSRIDHAR\t21\t8.5\n\nPRASAD\t22\t10.0");
+}
