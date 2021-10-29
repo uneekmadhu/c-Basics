84c64e85f12a1c565afe880d6237bcaef6ab2969
uneekmadhu <92910605+uneekmadhu@users.noreply.github.com>
2021-10-29 12:36:48 +0530

    Add files via upload

---
+++ PRCTICE.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+	//variabe declaration
+	int age;
+	float cgpa;
+	char initial;
+	
+	//initialization
+	age=20;
+	cgpa=7.20;
+	initial='R';
+	
+	//output
+	printf("Age of the student is %d \n",age);
+	printf("CGPA of the student is %f \n",cgpa);
+	printf("Student sur name starts with %c \n",initial);
+}
